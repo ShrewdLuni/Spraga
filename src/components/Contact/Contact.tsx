89586f081b7b9c7428cd0b6f8e756326713cc62f
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-06-14 22:13:41 +0300

    feat: finished base of contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,34 @@
+import { Button } from "../ui/button"
+import { Input } from "../ui/input"
+
 export const Contact = () => {
+  const handleSubmit = (e : any) => {
+    e.preventDefault();
+		e.stopPropagation();
+
+    const formData = new FormData(e.target);
+    const inputValue = formData.get('email');
+
+
+    console.log(inputValue);
+
+  };
+
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <p>Contact</p>
+    <div className="flex flex-row items-center justify-center w-screen h-screen">
+      <div className="flex items-center justify-center w-[50%]">Image</div>
+      <div className="flex items-center justify-center w-[50%]">
+        <form className="flex flex-col gap-y-2 justify-center h-full" onSubmit={handleSubmit}>
+          <div>
+            <p>Want to be Partners?</p>
+            <p>Fill the form and become one</p>
+          </div>
+          <Input type="text" placeholder="Name" name="name"/>
+          <Input type="number" placeholder="Phone" name="phone"/>
+          <Input type="email" placeholder="Email" name="email"/>
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
     </div>
   )
 }
