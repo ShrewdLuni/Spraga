bf1b437279d7b1c00f8e0652a830332ff2747e82
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-06-16 23:15:31 +0300

    feat: add about page template

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Partners } from './pages/Partners/Partners.tsx';
+import { About } from './pages/About/About.tsx';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +15,10 @@ const router = createBrowserRouter([
   {
     path: "/partners",
     element: <Partners/>,
+  },
+  {
+    path: "/about",
+    element: <About/>,
   },
 ]);
 
