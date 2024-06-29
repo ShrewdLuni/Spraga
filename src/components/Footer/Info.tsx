558bd165e2be8db2248359b89f1fc81cf6900cf4
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-06-29 16:55:16 +0300

    fix: footer bar display fix

--- src/components/Footer/Info.tsx
+++ src/components/Footer/Info.tsx
@@ -3,8 +3,8 @@ import { FooterItem } from "./FooterItem";
 export const FooterInfo = () => {
   return (
     <div className="flex flex-col w-full lg:w-auto justify-center lg:justify-start items-baseline gap-y-4 lg:gap-y-8">
-      <a href="/">
-        <p className="font-bold text-center w-full lg:w-auto text-4xl md:text-5xl xl:text-7xl lg:text-9xl transition-all duration-300">Spraga</p>
+      <a href="/" className="text-center w-full lg:w-auto">
+        <p className="font-bold lg:w-auto text-4xl md:text-5xl xl:text-7xl lg:text-9xl transition-all duration-300">Spraga</p>
       </a>
       <div className="flex flex-col w-full text-center lg:flex-row lg:justify-center gap-x-8">
         <FooterItem href="/about" label="About"/>
