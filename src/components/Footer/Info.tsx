b3bfad55e7762fe325c344f0fb0658e54e92ca1e
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-02 19:35:25 +0300

    refactor: footer

--- src/components/Footer/Info.tsx
+++ src/components/Footer/Info.tsx
@@ -1,16 +1,20 @@
 import { FooterItem } from "./FooterItem";
 
 export const FooterInfo = () => {
+  const infoItems = [
+    {label:"About",href:"/about"},
+    {label:"Shop",href:"/shop"},
+    {label:"Partners",href:"/partners"},
+    {label:"Payment and Delivery",href:"/aboutDelivery"},
+  ]
+
   return (
     <div className="flex flex-col w-full lg:w-auto justify-center lg:justify-start items-baseline gap-y-4 lg:gap-y-8">
       <a href="/" className="text-center w-full lg:w-auto">
         <p className="font-bold lg:w-auto text-4xl md:text-5xl xl:text-7xl lg:text-9xl transition-all duration-300">Spraga</p>
       </a>
       <div className="flex flex-col w-full text-center lg:flex-row lg:justify-center gap-x-8">
-        <FooterItem href="/about" label="About"/>
-        <FooterItem href="/shop" label="Shop"/>
-        <FooterItem href="/partners" label="Partners"/>
-        <FooterItem href="/aboutDelivery" label="Payment and Delivery"/>
+        {infoItems.map(item => <FooterItem href={item.href} label={item.label}/>)}
       </div>
     </div>
   )
