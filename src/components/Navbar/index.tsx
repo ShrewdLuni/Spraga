df6cd0441e8becf647b4186fc41682bb857d13a7
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-02 19:32:58 +0300

    reafactor: navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,13 @@ import { Button } from "../ui/button"
 import { NavbarItem } from "./NavbarItem"
 
 export const Navbar = () => {
+  const navbarItems = [
+    {label:"Home",href:"/"},
+    {label:"Shop",href:"/shop"},
+    {label:"About",href:"/about"},
+    {label:"Partners",href:"/partners"},
+  ]
+
   return (
     <div className="fixed flex flex-row flex-shrink items-baseline justify-between bg-transparent top-0 left-0 w-screen h-32 px-2 py-6 lg:px-20 z-50">
       <div className="flex-shrink-0">
@@ -11,10 +18,7 @@ export const Navbar = () => {
         </a>
       </div>
       <div className="flex-row gap-x-8 w-0 md:w-auto flex-shrink lg:w-auto overflow-hidden transition-all duration-300 hidden lg:flex">
-        <NavbarItem href="/" label="Home"/>
-        <NavbarItem href="/shop" label="Shop"/>
-        <NavbarItem href="/about" label="About"/>
-        <NavbarItem href="/partners" label="Partners"/>
+        {navbarItems.map(item => <NavbarItem href={item.href} label={item.label}/>)}
       </div>
       <div className="flex flex-row gap-x-2 flex-shrink-0">
         <Button className="bg-black rounded-full px-8 h-10 hidden lg:flex" variant="default">CART<ShoppingBag /></Button>
